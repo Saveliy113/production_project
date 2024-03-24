b6cf9fc921a2ca70cdec36231e403225162deeb6
Saveliy113 <sava2698@gmail.com>
2024-03-24 11:42:21 +0600

    Added router config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,5 @@
-import { Route, Routes } from 'react-router-dom';
 import './styles/index.scss';
 import { Link } from 'react-router-dom';
-import { Suspense } from 'react';
 import { useTheme } from '@/app/providers/ThemeProvider/lib/useTheme';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { AboutPage } from '@/pages/AboutPage';
@@ -15,12 +13,7 @@ const App = () => {
       <button onClick={toggleTheme}>Change Theme</button>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О сайте</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path={'/about'} element={<AboutPage />} />
-          <Route path={'/'} element={<MainPage />} />
-        </Routes>
-      </Suspense>
+      
     </div>
   );
 };
