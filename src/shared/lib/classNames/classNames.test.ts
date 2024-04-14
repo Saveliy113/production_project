13d9e940474f0954c431f8f02265338d6afeb71e
Saveliy <sava2698@gmail.com>
2024-04-14 21:01:09 +0600

    Added jest and jest config

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,34 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+  test('with only one first param', () => {
+    expect(classNames('someClass')).toBe('someClass');
+  });
+
+  test('with additional class', () => {
+    const expectedResult = 'someClass class1 class2';
+    expect(classNames('someClass', {}, ['class1', 'class2'])).toBe(
+      expectedResult,
+    );
+  });
+
+  test('with mods', () => {
+    const expectedResult = 'someClass class1 class2 hovered scrollable';
+    expect(
+      classNames('someClass', { hovered: true, scrollable: true }, [
+        'class1',
+        'class2',
+      ]),
+    ).toBe(expectedResult);
+  });
+
+  test('with mods false', () => {
+    const expectedResult = 'someClass class1 class2 hovered';
+    expect(
+      classNames('someClass', { hovered: true, scrollable: false }, [
+        'class1',
+        'class2',
+      ]),
+    ).toBe(expectedResult);
+  });
+});
--- src/test.ts
+++
@@ -1,5 +0,0 @@
-export function someFn(arg: number): string {
-  console.log('Random FN 123');
-
-  return '';
-}
