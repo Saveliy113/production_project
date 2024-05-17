26cbe3b9b4fcb8fb63c6a55b51b280433e40d0c9
Saveliy <sava2698@gmail.com>
2024-05-17 21:35:17 +0600

    fix eslint errors

--- src/app/providers/ThemeProvider/lib/ThemeContext.ts
+++ src/app/providers/ThemeProvider/lib/ThemeContext.ts
@@ -6,10 +6,10 @@ export enum Theme {
 }
 
 export interface ThemeContextProps {
-  theme?: Theme;
-  setTheme?: (theme: Theme) => void;
+  theme: Theme;
+  setTheme: (theme: Theme) => void;
 }
-
+// @ts-ignore
 export const ThemeContext = createContext<ThemeContextProps>({});
 
 export const LOCAL_STORAGE_THEME_KEY = 'theme';
--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -14,7 +14,11 @@ export function useTheme(): UseThemeResult {
 
   const toggleTheme = (): void => {
     const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
-    setTheme(newTheme);
+
+    if (setTheme) {
+      setTheme(newTheme);
+    }
+
     localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
   };
 
